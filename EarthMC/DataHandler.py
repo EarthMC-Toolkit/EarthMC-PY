ac1e012aff4d388962397e89e3435332013e7083
Owen Stubbs <owen77stubbs@gmail.com>
2023-08-21 20:42:50 +0100

    Update DataHandler.py

--- EarthMC/DataHandler.py
+++ EarthMC/DataHandler.py
@@ -5,14 +5,30 @@ from cachetools.func import ttl_cache
 
 class Endpoint:
     def __init__(self):
-       self.urls = self.reqJSON('https://raw.githubusercontent.com/EarthMC-Toolkit/Toolkit-Website/main/endpoints.json')
+        endpoints = "https://raw.githubusercontent.com/EarthMC-Toolkit/Toolkit-Website/main/endpoints.json"
+        self.urls = self.reqJSON(endpoints)
 
     @ttl_cache(2, 300)
-    def fetch(self, type, mapName): 
+    def fetch(self, type, mapName):
         return self.reqJSON(self.urls[type][mapName])
-    
+
     @staticmethod
-    def reqJSON(url):
+    def reqJSON(url: str):
         req = requests.get(url)
         try: return req.json()
         except: return FetchError("Error fetching endpoint: " + url + "\nResponse content is not valid JSON!")
+
+class OAPI:
+    def __init__(self, map = "aurora"):
+        self.domain = f"https://api.earthmc.net/v1/{map}"
+        self.urls = {
+            "towns": f"{self.domain}/towns",
+            "nations": f"{self.domain}/nations",
+            "residents": f"{self.domain}/residents",
+        }
+
+    def fetch_single(self, type: str, item = ''):
+        return Endpoint.reqJSON(self.urls[type] + "/" + item)
+
+    def fetch_all(self, type: str):
+        return Endpoint.reqJSON(self.urls[type])
