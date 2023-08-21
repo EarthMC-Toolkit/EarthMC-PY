62569a099e4f0ad0a2eb8c39aae0072b6952e958
Harrikar <97301242+Harrikar@users.noreply.github.com>
2023-08-21 22:29:29 +0300

    Update __init__.py

--- EarthMC/__init__.py
+++ EarthMC/__init__.py
@@ -1,6 +1,10 @@
 from .Classes.Nations import nations
 from .Classes.Players import players
 
+from .Classes.OAPI.Town import OAPI_Town
+from .Classes.OAPI.Nation import OAPI_Nation
+from .Classes.OAPI.Player import OAPI_Player
+
 from .DataHandler import OAPI
 from .Utils import utils, FetchError
 
@@ -33,7 +37,7 @@ class _OfficialAPI:
         self.api = OAPI(map)
 
     def town(self, name: str):
-         return self.api.fetch_single('towns', name)
+         return OAPI_Town(self.api.fetch_single('towns', name))
 
     def nation(self, name: str):
         return self.api.fetch_single('nations', name)
@@ -41,18 +45,34 @@ class _OfficialAPI:
     def player(self, name: str):
         return self.api.fetch_single('players', name)
 
-    # class Players:
-    #     def __init__(self, api: OAPI):
-    #         self.api = api
-    #
-    #     def all(self):
-    #         playerArr = []
-    #         playerList = self.api.fetch_all('residents')
-    #
-    #         for player in playerList['allResidents']:
-    #             playerArr.append(self.api.fetch_single('residents', player))
-    #
-    #         return playerArr
+    class Players:
+         def __init__(self, api: OAPI):
+            self.api = api
+
+         def all(self):
+             playerArr = []
+             playerList = self.api.fetch_all('residents')
+
+             for player in playerList['allResidents']:
+                 playerArr.append(self.api.fetch_single('residents', player))
+
+             return playerArr
+
+    class Nations:
+        class NationsPlayers:
+            def __init__(self, api: OAPI):
+                self.api = api
+
+            def all(self):
+                nationArr = []
+                nationList = self.api.fetch_all('nations')
+
+                for nation in nationList['allNations']:  
+                    nation_data = self.api.fetch_single('nations', nation)
+                    nationObj = OAPI_Nation(nation_data)
+                    nationArr.append(nationObj)
+
+                return nationArr
 
 
 OfficialAPI = _OfficialAPI()
