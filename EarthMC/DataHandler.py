d3b3cd51a3d9b8d6129016059e91894963545e00
Harrikar <97301242+Harrikar@users.noreply.github.com>
2023-08-22 16:35:50 +0300

    Update DataHandler.py

--- EarthMC/DataHandler.py
+++ EarthMC/DataHandler.py
@@ -1,7 +1,7 @@
-import requests
-
-from .Utils import FetchError
+import aiohttp
+import asyncio
 from cachetools.func import ttl_cache
+from .Utils import FetchError
 
 class Endpoint:
     def __init__(self):
@@ -9,17 +9,20 @@ class Endpoint:
         self.urls = self.reqJSON(endpoints)
 
     @ttl_cache(2, 300)
-    def fetch(self, type, mapName):
-        return self.reqJSON(self.urls[type][mapName])
+    async def fetch(self, type, mapName):
+        async with aiohttp.ClientSession() as session:
+            return await self.reqJSON(session, self.urls[type][mapName])
 
     @staticmethod
-    def reqJSON(url: str):
-        req = requests.get(url)
-        try: return req.json()
-        except: return FetchError("Error fetching endpoint: " + url + "\nResponse content is not valid JSON!")
+    async def reqJSON(session, url: str):
+        async with session.get(url) as response:
+            try:
+                return await response.json()
+            except aiohttp.ContentTypeError:
+                raise FetchError("Error fetching endpoint: " + url + "\nResponse content is not valid JSON!")
 
 class OAPI:
-    def __init__(self, map = "aurora"):
+    def __init__(self, map="aurora"):
         self.domain = f"https://api.earthmc.net/v1/{map}"
         self.urls = {
             "towns": f"{self.domain}/towns",
@@ -27,9 +30,10 @@ class OAPI:
             "residents": f"{self.domain}/residents",
         }
 
-    @ttl_cache(2, 5) # Cache it for 5 seconds to avoid spamming API.
-    def fetch_single(self, type: str, item = ''):
-        return Endpoint.reqJSON(self.urls[type] + "/" + item)
+    async def fetch_single(self, type: str, item=''):
+        async with aiohttp.ClientSession() as session:
+            return await Endpoint.reqJSON(session, self.urls[type] + "/" + item)
 
-    def fetch_all(self, type: str):
-        return Endpoint.reqJSON(self.urls[type])
+    async def fetch_all(self, type: str):
+        async with aiohttp.ClientSession() as session:
+            return await Endpoint.reqJSON(session, self.urls[type])
